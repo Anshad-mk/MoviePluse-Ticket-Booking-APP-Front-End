f7117b916287e06bd392445059c3c1ef67dc8bf7
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-04-27 19:12:27 +0530

    hosting17

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+outDir:'build',
+  },
   server:{
     port:3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
   }
 })
